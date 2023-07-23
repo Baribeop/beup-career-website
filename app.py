fa09516a1349594abf9f90c9e370f70f4d564912
Baribeop <68976451+Baribeop@users.noreply.github.com>
2023-07-23 21:17:34 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-# import required modules 
-from flask import Flask  # Flask is a class of the module flask
+# import required modules
+from flask import Flask, render_template  # Flask is a class of the module flask
 
 # instantiate Flask , create object of class Flask
-app =  Flask(__name__) # __name__ is the variable name known as __main__
+app = Flask(__name__)  # __name__ is the variable name known as __main__
 
 
 # create path/route for accessing web pages, @ is a decorator (advance python concept) which allows access to additional functionality
-#when the path / is followed, view function displays the return statement 
-@app.route('/') 
-def home(): 
-  return 'Welcome to Beup career website'
+#when the path / is followed, view function displays the return statement
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/apply')
+def form():
+  return render_template('form.html')
+
 
 # if the script app.py is run but not imported, the app should run
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', debug=True)
-  
-  
-
+  app.run(host='0.0.0.0', debug=True)
